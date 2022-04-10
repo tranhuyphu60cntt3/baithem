f33a2ee1683fa05014f63dd64f277f55eb412b49
phu <phu.th.60cntt@ntu.edu.vn>
2022-04-10 12:54:52 +0700

    10/4/2022

--- BaiKT2/app/src/main/java/tranhuyphu/baikt2/baikt2/MainActivity.java
+++ BaiKT2/app/src/main/java/tranhuyphu/baikt2/baikt2/MainActivity.java
@@ -3,12 +3,34 @@ package tranhuyphu.baikt2.baikt2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        radioGroup = findViewById(R.id.radG);
+        textView = findViewById(R.id.ViewTip);
+        Button button = findViewById(R.id.btntip);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int radID = radioGroup.getCheckedRadioButtonId();
+                radioButton = findViewById(radID);
+                textView.setText("Your tip will be: "+ radioButton.getText());
+            }
+        });
     }
+    public void checkbtn(View view) {
+        }
 }
