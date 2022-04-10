602ed7e20d12ce2d1523794eec52ee1280c2b34f
phu <phu.th.60cntt@ntu.edu.vn>
2022-04-10 16:59:13 +0700

    10/4/2022 lan 2

--- BaiKT2/app/src/main/java/tranhuyphu/baikt2/baikt2/MainActivity.java
+++ BaiKT2/app/src/main/java/tranhuyphu/baikt2/baikt2/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     RadioGroup radioGroup;
     RadioButton radioButton;
     TextView textView;
+    Button button;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,16 +24,31 @@ public class MainActivity extends AppCompatActivity {
 
         radioGroup = findViewById(R.id.radG);
         textView = findViewById(R.id.ViewTip);
-        Button button = findViewById(R.id.btntip);
+        editText = findViewById(R.id.edtMoney);
+        button = findViewById(R.id.btntip);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int radID = radioGroup.getCheckedRadioButtonId();
-                radioButton = findViewById(radID);
-                textView.setText("Your tip will be: "+ radioButton.getText());
+                String tip = editText.getText().toString();
+                float money = Float.parseFloat(tip);
+                int radioId = radioGroup.getCheckedRadioButtonId();
+                radioButton = findViewById(radioId);
+                String s = (String) radioButton.getText();
+                if (s.equals("13%")){
+                    money = money * 13 /100;
+                }else if (s.equals("15%")){
+                    money = money * 15 /100;
+                }else{
+                    money = money * 18 /100;
+                }
+                textView.setText("Your tip will be: $"+ money);
             }
         });
     }
     public void checkbtn(View view) {
+        int radID = radioGroup.getCheckedRadioButtonId();
+        radioButton = findViewById(radID);
+        Toast.makeText(this, "You select tip: " + radioButton.getText(),
+                Toast.LENGTH_SHORT).show();
         }
 }
